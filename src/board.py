33cd735c2ca7002069535ed3fb1e67b0388567fc
jpetersen <jon.petersen@web.de>
2025-06-01 23:09:14 +0200

    uncover single cells
    added method to convert from world 2 grid coordinates

--- src/board.py
+++ src/board.py
@@ -1,10 +1,12 @@
 """Board class to handle cell management."""
 
 import itertools
+from functools import lru_cache
 
 import pygame
 
 from cell import Cell
+
 
 class Board:
     """Board class to handle cell management."""
@@ -35,13 +37,28 @@ class Board:
         self.board = self.init_board()
 
     def init_board(self) -> list[list[Cell]]:
+        """Initialize the board."""
         return [[Cell(x, y, self.cell_size) for x in range(self.width)] for y in range(self.height)]
 
     @property
+    @lru_cache
     def cell_size(self) -> tuple[int, int]:
+        """Cell size of the board."""
         return self.screen_width // self.width, self.screen_height // self.height
 
     def draw(self, surface: pygame.Surface) -> None:
         """Draw all cells of the board on the surface."""
         for cell in itertools.chain(*self.board):
             cell.draw(surface)
+
+    def world2grid(self, position: tuple[int, int]) -> tuple[int, int]:
+        """Calculate from world position / pixel to grid coordinates."""
+        return position[0] // self.cell_size[0], position[1] // self.cell_size[1]
+
+    def cell_at_position(self, x: int, y: int) -> Cell:
+        return self.board[y][x]
+
+    def uncover(self, mouse_pos: tuple[int, int]) -> None:
+        grid_x, grid_y = self.world2grid(mouse_pos)
+        target_cell = self.cell_at_position(grid_x, grid_y)
+        target_cell.uncover()
--- src/main.py
+++ src/main.py
@@ -36,13 +36,8 @@ def main():
                 continue
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                # ToDo use mouse pos to figure out which cell
-                # is pressed
-                # mouse_pos = pygame.mouse.get_pos()
-                # cell_x = mouse_pos[0] // cell_size
-                # cell_y = mouse_pos[1] // cell_size
-                # cell.uncover()
-                ...
+                mouse_pos = pygame.mouse.get_pos()
+                board.uncover(mouse_pos)
 
         screen.fill((200, 200, 200))
 

dd431398eaf9d3b33a8ce1a680b6fc11e37d1b2c
jpetersen <jon.petersen@web.de>
2025-06-01 21:57:59 +0200

    create basic pygame window

---
+++ src/main.py
@@ -0,0 +1,32 @@
+"""Main module to run the game from."""
+import pygame
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+
+def main():
+    """Main function to run the game from."""
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('Minesweeper')
+
+    clock = pygame.time.Clock()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((200, 200, 200))
+
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
